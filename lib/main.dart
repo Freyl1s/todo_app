abf661aaa43e8bb2226dde734abb7e764fc9f480
freylis <freylis82@gmail.com>
2024-05-03 13:06:43 +0200

    Integration WebScaper

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/foundation.dart';
 
+import 'package:web_scraper/web_scraper.dart';
+//import 'package:html/dom.dart';
+//import 'package:html/parser.dart'; // Contains HTML parsers to generate a Document object.
+import 'package:http/http.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
@@ -26,21 +31,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(
             seedColor: const Color.fromARGB(255, 42, 176, 47)),
         useMaterial3: true,
@@ -52,15 +42,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -125,6 +106,25 @@ class _MyHomePageState extends State<MyHomePage> {
                         password: _passwortController.text);
                   },
                   child: Text('Register')),
+
+              SizedBox(
+                height: 20,
+              ),
+              //Register Button
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(minimumSize: Size(200, 50)),
+                  onPressed: () async {
+                    final webScraper = WebScraper('https://webscraper.io');
+                    if (await webScraper
+                        .loadWebPage('/test-sites/e-commerce/allinone')) {
+                      List<Map<String, dynamic>> elements = webScraper
+                          .getElement('h1.title > a.caption', ['href']);
+
+                      print("Daten kommen von der Seite");
+                      print(elements);
+                    }
+                  },
+                  child: Text('Daten holen')),
             ],
           ),
         ));

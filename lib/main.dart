85384c435f08eec1153c48489b75a9cb3f569d4d
freylis <freylis82@gmail.com>
2024-05-06 20:35:55 +0200

    Vesuch mit Git Reset

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'dart:html';
+
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:flutter/foundation.dart';
+import 'package:http/http.dart';
+import 'package:html/dom.dart';
+//import 'package:html/parser.dart';
+//import 'package:web_scraper/web_scraper.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,6 +20,7 @@ void main() async {
   runApp(const MyApp());
 }
 
+
 class MyApp extends StatelessWidget {
   //debugPrint("Das ist ein Test");
   const MyApp({super.key});
@@ -26,21 +31,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(
             seedColor: const Color.fromARGB(255, 42, 176, 47)),
         useMaterial3: true,
@@ -53,15 +43,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -71,6 +52,11 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwortController = TextEditingController();
+  
+}
+
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -125,6 +111,38 @@ class _MyHomePageState extends State<MyHomePage> {
                         password: _passwortController.text);
                   },
                   child: Text('Register')),
+
+              SizedBox(
+                height: 20,
+              ),
+              //Register Button
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(minimumSize: Size(200, 50)),
+                  onPressed: () async {
+                    
+                    print("Button gedrückt")
+
+                   Future getWebsiteDate() async {
+                    final url = Uri.parse('https://www.hb9scbo.ch/de/rufzeichenliste.htm');
+                    final response = await http.get(url);
+                    dom.Document html = dom.Document.html(repsonse.body);
+
+                    final titles = html
+                    .querySelectorAll('#content > table')
+                    .map((element) => element.innerHtml.trim())
+                    .toList();
+
+                    print('Count: ${titles.length}');
+                    for (final title in titles) {
+                    debugPrint(titles);
+                    }
+
+
+                    print("Button Ende")
+                    
+                    }
+                  },
+                  child: Text('Daten holen')),
             ],
           ),
         ));
